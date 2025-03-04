bed631e1810303c7c5ba6672097a99f48a020e8f
Alexander <aarashidov@gmail.com>
2025-03-04 16:47:35 -0300

    Katya's code edits

--- Assets/Madhouse/Scripts/Madhouse.BipolarDisorder/InputController.cs
+++ Assets/Madhouse/Scripts/Madhouse.BipolarDisorder/InputController.cs
@@ -2,26 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputController : MonoBehaviour
-{
-    private SpriteRenderer spriteRenderer;
-    private Color whiteColor = Color.white;
-    private Color blackColor = Color.black;
- 
-    // Start is called before the first frame update
-    void Start()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.color = whiteColor; // Начальный цвет - белый    
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace Madhouse.BipolarDisorder
+{
+
+    public class InputController : MonoBehaviour
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        private SpriteRenderer _spriteRenderer;
+        private Color _whiteColor = Color.white;
+        private Color _blackColor = Color.black;
+ 
+   
+        void Start()
         {
-            spriteRenderer.color = (spriteRenderer.color == whiteColor) ? blackColor : whiteColor;
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _spriteRenderer.color = _whiteColor;   
+        }
+
+   
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _spriteRenderer.color = (_spriteRenderer.color == _whiteColor) ? _blackColor : _whiteColor;
+            }
         }
     }
+
 }
-//TODO: fix
